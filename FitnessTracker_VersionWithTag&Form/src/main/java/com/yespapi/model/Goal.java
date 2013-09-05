ca04d7e48bd83243ae757e15cb241ea169705f8d
aly ba <alybas2002@yahoo.fr>
2013-09-05 15:33:13 +0200

    springmvc work
    extension about using tags and forms in springmvc

---
+++ FitnessTracker_VersionWithTag&Form/src/main/java/com/yespapi/model/Goal.java
@@ -0,0 +1,15 @@
+package com.yespapi.model;
+
+public class Goal {
+	
+	public int minutes;
+
+	public int getMinutes() {
+		return minutes;
+	}
+
+	public void setMinutes(int minutes) {
+		this.minutes = minutes;
+	}
+
+}
